7e01f72f3cc0ea1953fd1076d98d6f075a22a0f1
ragunadevi <ranjithamurali@gmail.com>
2022-05-17 19:47:50 +0530

    Update MyCalculator.java

--- week1/day2/MyCalculator.java
+++ week1/day2/MyCalculator.java
@@ -8,6 +8,6 @@ public class MyCalculator
 		System.out.println("Addition: " + calculator.add(20, 12, 30)) ;
 		System.out.println("Subtraction: " + calculator.sub(40, 12)) ;
 		System.out.println("Multiplication: " + calculator.multiply(20, 45)) ;
-		System.out.println("Division: " + calculator.divide(100, 3)) ;
+		System.out.println("Division: " + calculator.divide(99, 3)) ;
 	}
 }
